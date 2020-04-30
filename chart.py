c83e410c0376efd9e0fafefdd7a0400c80bb8de9
Jackson-S <Jackson-S@users.noreply.github.com>
2020-04-30 23:01:54 +1000

    Convert to Flask app

--- chart.py
+++ chart.py
@@ -8,8 +8,12 @@ from dataclasses import dataclass
 from time import localtime, strftime, time
 from datetime import datetime, timedelta
 from typing import List, Optional
+
+from flask import Flask, render_template
+
 from settings import *
 
+app=Flask(__name__)
 
 @dataclass
 class Dataset:
@@ -19,6 +23,8 @@ class Dataset:
 
 
 def fetch_data_hourly(time_range: timedelta) -> Dataset:
+    db = sqlite3.connect(DATABASE_LOCATION)
+    cursor = db.cursor()
     earliest_time = (datetime.utcnow() - time_range).strftime("%Y-%m-%d %H:%M:%S")
     query_string = """
     SELECT DATETIME(timestamp, 'localtime'), temperature, humidity 
@@ -27,7 +33,9 @@ def fetch_data_hourly(time_range: timedelta) -> Dataset:
       ORDER BY timestamp
     """
     query = cursor.execute(query_string, (earliest_time, ))
-    results = list(query.fetchall())        
+    results = list(query.fetchall())   
+    cursor.close()
+    db.close()     
     timestamps = [datetime.fromisoformat(x[0]) for x in results]
     temperature = [x[1] for x in results]
     humidity = [x[2] for x in results]
@@ -35,6 +43,8 @@ def fetch_data_hourly(time_range: timedelta) -> Dataset:
 
 
 def fetch_data(time_range: timedelta) -> Dataset:
+    db = sqlite3.connect(DATABASE_LOCATION)
+    cursor = db.cursor()
     earliest_time = (datetime.utcnow() - time_range).strftime("%Y-%m-%d %H:%M:%S")
     query_string = """
     SELECT DATETIME(timestamp, 'localtime'), temperature, humidity 
@@ -43,7 +53,9 @@ def fetch_data(time_range: timedelta) -> Dataset:
       ORDER BY timestamp
     """
     query = cursor.execute(query_string, (earliest_time, ))
-    results = list(query.fetchall())        
+    results = list(query.fetchall())
+    cursor.close()
+    db.close()
     timestamps = [datetime.fromisoformat(x[0]) for x in results]
     temperature = [x[1] for x in results]
     humidity = [x[2] for x in results]
@@ -78,44 +90,29 @@ def format_data(data: List[Optional[float]]) -> str:
     result = "[" + ",".join(result) + "]"
     return result
 
-
-if __name__ == "__main__":
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("-t", "--type", action="store", default="small", type=str)
-    args = arg_parser.parse_args()
-
-    # Open the Database
-    db = sqlite3.connect(DATABASE_LOCATION)
-    cursor = db.cursor()
-
-    for property_list in OUTPUT_PROPERTIES:
-        if property_list["name"] == args.type:
-            properties = property_list
+@app.route("/")
+@app.route("/<length>")
+def root_page(length="small"):
+    properties = None
+    for property in OUTPUT_PROPERTIES:
+        if property["name"] == length:
+            properties = property
             break
     else:
-        print("Unknown output type \"{}\"".format(args.type), file=sys.stderr)
-        sys.exit(1)
-
-    # Convert the timeframe into a time delta object
+        return "Unknown Length"
+    
+    # Convert the hours into a timeframe object
     timeframe = timedelta(hours=properties["duration"])
-
-    # Read the template output file
-    with open(TEMPLATE_LOCATION) as in_file:
-        chart_data = in_file.read()
-
+    
+    # Fetch the data
     if properties["detail_level"] == "fine":
         data = fetch_data(timeframe)
     else:
         data = fetch_data_hourly(timeframe)
-    
-    chart_data = chart_data.replace("{{ properties }}", json.dumps(OUTPUT_PROPERTIES))
-    chart_data = chart_data.replace("{{ uptime }}", strftime("%Y-%m-%d %I:%M %p", localtime()))
-    chart_data = chart_data.replace("{{ temperature }}", format_data(data.temperature))
-    chart_data = chart_data.replace("{{ humidity }}", format_data(data.humidity))
-    chart_data = chart_data.replace("{{ labels }}", format_data(format_timestamps(data.timestamp)))
 
-    with open(properties["location"], "w") as out_file:
-        out_file.write(chart_data)
-
-    cursor.close()
-    db.close()
+    return render_template("chart.html", 
+        properties=OUTPUT_PROPERTIES, 
+        temperature=format_data(data.temperature),
+        humidity=format_data(data.humidity),
+        labels=format_timestamps(data.timestamp),
+        uptime=strftime("%Y-%m-%d %I:%M %p", localtime()))
--- settings.py
+++ settings.py
@@ -1,11 +1,10 @@
 from os import path, getenv
 
 # The IP Address or URL of the sensor
-SENSOR_ADDRESS = getenv(["SENSOR_ADDRESS"], "127.0.0.1")
+SENSOR_ADDRESS = getenv("SENSOR_ADDRESS", "127.0.0.1")
 
 # The base directory for both input and output files
-OUTPUT_DIRECTORY = getenv(["OUTPUT_DIRECTORY"], "./")
-INPUT_DIRECTORY = getenv(["INPUT_DIRECTORY"], "./")
+INPUT_DIRECTORY = getenv("INPUT_DIRECTORY", "/logger/")
 
 # Locations of input files
 TEMPLATE_LOCATION = path.join(INPUT_DIRECTORY, "chart.html")
@@ -25,32 +24,24 @@ DATABASE_LOCATION = path.join(INPUT_DIRECTORY, "temperatures.db")
 OUTPUT_PROPERTIES = [
     {
         "name": "small",
-        "location": path.join(OUTPUT_DIRECTORY, "chart.html"),
-        "url": "chart.html",
         "duration": 24,
         "duration_string": "1 Day",
         "detail_level": "fine"
     },
     {
         "name": "medium",
-        "location": path.join(OUTPUT_DIRECTORY, "chart_medium.html"),
-        "url": "chart_medium.html",
         "duration": 168,
         "duration_string": "1 Week",
         "detail_level": "coarse"
     },
     {
         "name": "large",
-        "location": path.join(OUTPUT_DIRECTORY, "chart_large.html"),
-        "url": "chart_large.html",
         "duration": 672,
         "duration_string": "1 Month",
         "detail_level": "coarse"
     },
     {
         "name": "year",
-        "location": path.join(OUTPUT_DIRECTORY, "chart_year.html"),
-        "url": "chart_year.html",
         "duration": 8760,
         "duration_string": "1 Year",
         "detail_level": "coarse"
