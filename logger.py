9f5ea079e2d5b981f0200ca09c5eba8d6284c3e1
Jackson-S <Jackson-S@users.noreply.github.com>
2020-05-01 19:26:01 +1000

    Rewrite logger to be more resistant to crashes and update on only the major 5 minute periods

--- logger.py
+++ logger.py
@@ -1,65 +1,186 @@
 #!/usr/bin/python3
 
+import os
 import sys
+import math
 import json
+import time
 import sqlite3
+
+from typing import Optional, Iterable
+from datetime import datetime, timedelta
+from threading import Timer
+from dataclasses import dataclass
+
 import requests
-import threading
-from settings import *
 
-class DatabaseRecorder(threading.Thread):
-    def __init__(self, event: threading.Event, period: int):
-        threading.Thread.__init__(self)
-        self.stopped = event
-        self.period = period
-    
+
+@dataclass
+class SensorData:
+    temperature: Optional[float]
+    humidity: Optional[float]
+
+
+class RepeatTimer(Timer):
     def run(self):
-        self.call()
-        while not self.stopped.wait(self.period):
-            self.call()
-            
-    def call(self):
-            try:
-                if not record():
-                    print("Unknown error occurred")
-                else:
-                    print("Success")
-            except requests.Timeout:
-                print("No response received from server")
-            except BaseException as e:
-                print("Unknown error occurred:")
-                print(e)
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
 
-def record():
-    db = sqlite3.connect(DATABASE_LOCATION)
-    cursor = db.cursor()
 
-    temperature = None
-    humidity = None
+def fetch_data() -> Optional[SensorData]:
+    sensor_address = os.getenv("SENSOR_ADDRESS")
 
-    query_text = "INSERT INTO Recordings (temperature, humidity) VALUES (?, ?)"
+    if not sensor_address:
+        print("SENSOR_ADDRESS environment variable is not set", file=sys.stderr)
+        return None
 
-    response = requests.get("http://{}/".format(SENSOR_ADDRESS), timeout=2)
+    sensor_url = f"http://{sensor_address}/"
 
-    if response.status_code == requests.codes.ok:
-        response_json = json.loads(response.content)
-        
-        if (type(response_json) is dict) and ("temperature" and "humidity" in response_json):
-            temperature = response_json["temperature"]
-            humidity = response_json["humidity"]
-        else:
-            print("Invalid response received from server: \n\"{}\"".format(response.content))
+    try:
+        response = requests.get(sensor_url, timeout=5)
+    except requests.ConnectionError:
+        print(f"Failed to connect to resource {sensor_url}", file=sys.stderr)
+        return None
+
+    if not response.status_code is requests.codes.ok:
+        print(f"Got response code {response.status_code}", file=sys.stderr)
+        return None
+
+    try:
+        response_content = json.loads(response.content)
+    except json.JSONDecodeError:
+        print("Unable to decode JSON data from server", file=sys.stderr)
+        return None
+
+    if not isinstance(response_content, dict):
+        print("Invalid response type", file=sys.stderr)
+        return None
+
+    result = SensorData(
+        response_content.get("temperature", None),
+        response_content.get("humidity", None)
+    )
+
+    return result
+
+
+def update_database(query: str, arguments: Iterable) -> bool:
+    database_location = os.getenv("DATABASE_LOCATION")
+
+    if not database_location:
+        print("SENSOR_ADDRESS environment variable is not set", file=sys.stderr)
+        return False
+
+    if not os.path.isfile(database_location):
+        # Create the database if it doesn't exist
+        try:
+            _create_database(database_location)
+        except sqlite3.DatabaseError as error:
+            print(f"Error initialising database:\n{error}", file=sys.stderr)
             return False
 
-    cursor.execute(query_text, (temperature, humidity))
+    database = sqlite3.connect(database_location)
+    cursor = database.cursor()
+
+    try:
+        cursor.execute(query, arguments)
+    except sqlite3.DatabaseError as error:
+        print(f"Error inserting into database:\n{error}", file=sys.stderr)
+        cursor.close()
+        database.close()
+        return False
 
     cursor.close()
-    db.commit()
-    db.close()
-
+    database.commit()
+    database.close()
     return True
 
-stopFlag = threading.Event()
-# Set the timer for 5 minute intervals
-thread = DatabaseRecorder(stopFlag, 5 * 60)
-thread.start()
+
+def _create_database(path: os.path):
+    # Connect to the database (will create the database in the process)
+    database = sqlite3.connect(path)
+    cursor = database.cursor()
+
+    # Initialise the database
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS Recordings (\n" + \
+        "  timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,\n" + \
+        "  temperature FLOAT,\n" + \
+        "  humidity FLOAT\n" + \
+        ");"
+    )
+
+    cursor.close()
+
+    database.commit()
+    database.close()
+
+
+def record_sensor_data() -> None:
+    sensor_data = fetch_data()
+
+    if not sensor_data:
+        return
+
+    query = "INSERT INTO Recordings (temperature, humidity) VALUES (?, ?)"
+    arguments = [sensor_data.temperature, sensor_data.humidity]
+
+    # Record the data
+    if update_database(query, arguments):
+        print(f"{datetime.now()}: Recorded temp: {arguments[0]}, humidity: {arguments[1]}.")
+
+
+def get_start_time_delta() -> int:
+    # Calculate the start of the next even 5 minute period
+    current_time = datetime.now()
+
+    next_clock_numeral = (math.ceil(current_time.minute / 5) * 5) % 60
+    initialisation_time = current_time.replace(minute=next_clock_numeral)
+
+    # If the time is (55, 59] then the first recording will happen in the next hour
+    if next_clock_numeral == 0:
+        next_hour = current_time.hour + 1
+        initialisation_time = initialisation_time.replace(hour=next_hour)
+    
+    # Make up the difference in the sub-minute intervals
+    sub_minute_delta = timedelta(seconds=current_time.second, microseconds=current_time.microsecond)
+    initialisation_time -= sub_minute_delta
+
+    # If the current minutes is a multiple of 5 then we have to go to the next 5 minute period
+    if current_time.minute % 5 == 0:
+        initialisation_time += timedelta(minutes=5)
+
+    # I'm not confident in my algorithm for this. 
+    # I'd rather it shuts down than never runs so assert this and hope for a crash.
+    if initialisation_time < datetime.now():
+        print("Invalid time calculated. This is a bug.")
+        print(initialisation_time, datetime.now())
+        sys.exit(1)
+
+    return (initialisation_time - datetime.now()).total_seconds()
+
+
+if __name__ == "__main__":
+    # Check the environment
+    if not os.getenv("SENSOR_ADDRESS"):
+        print("SENSOR_ADDRESS environment variable is not set")
+        sys.exit(1)
+
+    if not os.getenv("DATABASE_LOCATION"):
+        print("DATABASE_LOCATION environment variable is not set")
+        sys.exit(1)
+
+    # Wait until the interval
+    start_time_delta = get_start_time_delta()
+
+    start_time = datetime.now() + timedelta(seconds=start_time_delta)
+    print(f"Will begin recording data every 5 minutes in {start_time_delta} seconds. {start_time.time()}")
+
+    time.sleep(start_time_delta)
+
+    # Do an initial recording
+    record_sensor_data()
+
+    # Set a timer to repeat recordings every 5 minutes
+    timer = RepeatTimer(5 * 60, record_sensor_data)
+    timer.start()
