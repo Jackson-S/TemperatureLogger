ac93cae594ca2e404ffa6b7bf8471bb2f97cc1ad
Jackson-S <Jackson-S@users.noreply.github.com>
2020-05-01 11:13:42 +1000

    Stop logger reliance on cronjobs

--- logger.py
+++ logger.py
@@ -4,34 +4,60 @@ import sys
 import json
 import sqlite3
 import requests
+import threading
 from settings import *
 
-db = sqlite3.connect(DATABASE_LOCATION)
-cursor = db.cursor()
-
-temperature = None
-humidity = None
-
-query_text = "INSERT INTO Recordings (temperature, humidity) VALUES (?, ?)"
-
-try:
-    response = requests.get("http://{}/".format(SENSOR_ADDRESS), timeout=2)
-except requests.Timeout:
-    print("No response received from server.", file=sys.stderr)
-    cursor.execute(query_text, (temperature, humidity))
-    exit(1)
-
-if response.status_code == requests.codes.ok:
-    response_json = json.loads(response.content)
+class DatabaseRecorder(threading.Thread):
+    def __init__(self, event: threading.Event, period: int):
+        threading.Thread.__init__(self)
+        self.stopped = event
+        self.period = period
     
-    if (type(response_json) is dict) and ("temperature" and "humidity" in response_json):
-        temperature = response_json["temperature"]
-        humidity = response_json["humidity"]
-    else:
-        print("Invalid response received from server: \n\"{}\"".format(response.content), file=sys.stderr)
+    def run(self):
+        self.call()
+        while not self.stopped.wait(self.period):
+            self.call()
+            
+    def call(self):
+            try:
+                if not record():
+                    print("Unknown error occurred")
+                else:
+                    print("Success")
+            except requests.Timeout:
+                print("No response received from server", file=sys.stderr)
+            except BaseException as e:
+                print("Unknown error occurred:")
+                print(e)
 
-cursor.execute(query_text, (temperature, humidity))
+def record():
+    db = sqlite3.connect(DATABASE_LOCATION)
+    cursor = db.cursor()
 
-cursor.close()
-db.commit()
-db.close()
+    temperature = None
+    humidity = None
+
+    query_text = "INSERT INTO Recordings (temperature, humidity) VALUES (?, ?)"
+
+    response = requests.get("http://{}/".format(SENSOR_ADDRESS), timeout=2)
+
+    if response.status_code == requests.codes.ok:
+        response_json = json.loads(response.content)
+        
+        if (type(response_json) is dict) and ("temperature" and "humidity" in response_json):
+            temperature = response_json["temperature"]
+            humidity = response_json["humidity"]
+        else:
+            print("Invalid response received from server: \n\"{}\"".format(response.content), file=sys.stderr)
+
+    cursor.execute(query_text, (temperature, humidity))
+
+    cursor.close()
+    db.commit()
+    db.close()
+
+    return True
+
+stopFlag = threading.Event()
+thread = DatabaseRecorder(stopFlag, 5)
+thread.start()
