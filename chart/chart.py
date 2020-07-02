1732e2d7fbec71dd1592d4c4e08df5653048e21c
Jackson Sommerich <Jackson-S@users.noreply.github.com>
2020-07-02 19:42:05 +1000

    Update chart app

--- chart/chart.py
+++ chart/chart.py
@@ -1,99 +1,88 @@
 #!/usr/bin/python3
 
+import os
 import sys
 import sqlite3
 from dataclasses import dataclass
 from time import localtime, strftime, time
 from datetime import datetime, timedelta
-from typing import List, Optional
+from typing import List, Optional, Any
 
 from flask import Flask, render_template
-
-from settings import *
 
 app=Flask(__name__)
 
 @dataclass
 class Dataset:
-    timestamp: List[datetime]
-    temperature: List[Optional[float]]
-    humidity: List[Optional[float]]
+    time: List[datetime]
+    value: List[float]
 
 
-def fetch_data_hourly(time_range: timedelta) -> Dataset:
-    db = sqlite3.connect(DATABASE_LOCATION)
-    cursor = db.cursor()
-    earliest_time = (datetime.utcnow() - time_range).strftime("%Y-%m-%d %H:%M:%S")
-    query_string = """
-    SELECT DATETIME(timestamp, 'localtime'), temperature, humidity 
-      FROM Recordings
-      WHERE timestamp >= ? AND timestamp LIKE '%:00:%'
-      ORDER BY timestamp
-    """
-    query = cursor.execute(query_string, (earliest_time, ))
-    results = list(query.fetchall())   
+def database_query(query: str, parameters: List[Any]) -> List[Any]:
+    # Get the database location from the OS environment (should be set by docker)
+    database_location = os.getenv("DATABASE_LOCATION")
+
+    database = sqlite3.connect(database_location)
+    cursor = database.cursor()
+    query = cursor.execute(query)
+    result = query.fetchall()
     cursor.close()
-    db.close()     
-    timestamps = [datetime.fromisoformat(x[0]) for x in results]
-    temperature = [x[1] for x in results]
-    humidity = [x[2] for x in results]
-    return Dataset(timestamps, temperature, humidity)
+    database.close()
 
-
-def fetch_data(time_range: timedelta) -> Dataset:
-    db = sqlite3.connect(DATABASE_LOCATION)
-    cursor = db.cursor()
-    earliest_time = (datetime.utcnow() - time_range).strftime("%Y-%m-%d %H:%M:%S")
-    query_string = """
-    SELECT DATETIME(timestamp, 'localtime'), temperature, humidity 
-      FROM Recordings
-      WHERE timestamp >= ?
-      ORDER BY timestamp
-    """
-    query = cursor.execute(query_string, (earliest_time, ))
-    results = list(query.fetchall())
-    cursor.close()
-    db.close()
-    timestamps = [datetime.fromisoformat(x[0]) for x in results]
-    temperature = [x[1] for x in results]
-    humidity = [x[2] for x in results]
-    return Dataset(timestamps, temperature, humidity)
-
-
-def format_timestamps(timestamps: List[datetime]) -> List[str]:
-    current_time = datetime.utcnow()
-    least_recent_timestamp = min(timestamps)
-    # The format string for < 24 hours (i.e. "1:30 PM")
-    format_string = "%I:%M %p"
-    if current_time - least_recent_timestamp >= timedelta(hours=24):
-        # The format string for 144 < x <= 24 (i.e. "Mon 01:30 PM")
-        format_string = "%a %-I %p"
-    if current_time - least_recent_timestamp >= timedelta(hours=144):
-        # The format string for >144 hours (i.e. "2020/01/01 13:30")
-        format_string = "%Y/%m/%d %H:%M"
-    result = [x.strftime(format_string) for x in timestamps]
     return result
 
 
-@app.route("/")
-@app.route("/<length>")
-def root_page(length="small"):
-    properties = None
-    for property in OUTPUT_PROPERTIES:
-        if property["name"] == length:
-            properties = property
-            break
-    else:
-        return "Unknown Length"
+def create_dataset(data: List[List[str, str]]) -> Dataset:
+    times = [datetime.fromisoformat(x[0]) for x in data]
+    values = [x[1] for x in data]
+    return Dataset(times, values)
+
+
+def fetch_data(range: timedelta, datatype: str, device: str) -> Dataset:
+    query = """
+    SELECT DATETIME(time, 'localtime'), value 
+        FROM Responses
+        WHERE device = ? 
+            AND type = ?
+            AND timestamp >= ?;
+    """
+
+    # Convert the range we want to the least recent date in the range, and convert it to a string format SQLite understands    
+    earliest_time = (datetime.utcnow() - range).strftime("%Y-%m-%d %H:%M:%S")
+
+    parameters = [device, datatype, earliest_time]
+
+    query_response = database_query(query, parameters)
+
+    return create_dataset(query_response)
+
+
+def pretty_print_timestamps(timestamp_list: List[datetime]) -> List[str]:
+    # Get the range of times covered by the list
+    max_time_delta = datetime.utcnow() - min(timestamp_list)
     
-    # Convert the hours into a timeframe object
-    timeframe = timedelta(hours=properties["duration"])
+    if max_time_delta >= timedelta(weeks=1):
+        timestamp_format = "%Y/%m/%d %H:%M"
+    elif max_time_delta >= timedelta(days=1):
+        timestamp_format = "%a %-I %p"
+    else:
+        timestamp_format = "%I:%M %p"
+
+    # Convert the times to the format string decided above and return
+    return [x.strftime(timestamp_format) for x in timestamp_list]
+
+
+@app.route("/")
+def root_page():
+    # Get the time delta in hours (default to 24)
+    timeframe = timedelta(hours=request.args.get("timeframe", default=24, type=int))
+    
+    datatype = request.args.get("datatype", default="temperature", type=str)
+    
+    device = request.args.get("device", default=0, type=int)
     
     # Fetch the data
-    if properties["detail_level"] == "fine":
-        data = fetch_data(timeframe)
-    else:
-        data = fetch_data_hourly(timeframe)
+    dataset = fetch_data(timeframe, datatype, device)
 
     return render_template("chart.html", 
         properties=OUTPUT_PROPERTIES, 
